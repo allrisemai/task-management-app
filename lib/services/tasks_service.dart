02d04badc4d8431bcd1620acc9f98789dc4be8a5
Yossawan Ngernpool <allrisemarch@gmail.com>
2023-09-28 13:20:05 +0700

    update network

---
+++ lib/services/tasks_service.dart
@@ -0,0 +1,49 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:task_management_app/models/task_list_model.dart';
+import 'package:task_management_app/services/dio_client.dart';
+import 'package:task_management_app/services/dio_exception.dart';
+
+class TasksService {
+  final _dio = DioClient();
+
+  Future<TaskList?> getTaskList({
+    int? offset,
+    int? limit,
+    String? status,
+    String? sortBy,
+    bool? isAsc,
+  }) async {
+    try {
+      final params = status != null
+          ? <String, dynamic>{
+              "offset": offset ?? 0,
+              "limit": limit ?? 10,
+              'sortBy': sortBy ?? '',
+              "isAsc": isAsc ?? true,
+              "status": status
+            }
+          : <String, dynamic>{
+              "offset": offset ?? 0,
+              "limit": limit ?? 10,
+              'sortBy': sortBy ?? '',
+              "isAsc": isAsc ?? true,
+            };
+      final response = await _dio.get('/todo-list', queryParameters: params
+          // '?offset=${offset ?? 0}&limit=${limit ?? 10}${sortBy != null ? "&sortBy=$sortBy" : ""}&isAsc=${isAsc ?? true}${status != null ? '&status=$status' : ""}'
+          );
+      return TaskList.fromJson(response);
+    } on DioException catch (err) {
+      final errorMessage = CustomDioException.fromDioError(err).toString();
+      if (kDebugMode) {
+        print(err.response?.data['message']);
+        print(errorMessage);
+      }
+      return null;
+      // throw errorMessage;
+    } catch (e) {
+      if (kDebugMode) print(e);
+      throw e.toString();
+    }
+  }
+}
